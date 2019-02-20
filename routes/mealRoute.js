a6d32e3723d42d22522fe88043904fadd47cf071
Oyekunle-Mark <oyekunlemac@gmail.com>
2019-02-20 02:00:28 +0100

    Added documentation

--- routes/mealRoute.js
+++ routes/mealRoute.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import MealController from '../model/mealController';
 import MealMiddleware from '../middlewares/mealMiddleware';
+import swaggerDocument from '../swagger.json';
 
 const mealRoute = Router();
+
+mealRoute.use('/', swaggerUi.serve);
+mealRoute.get('/', swaggerUi.setup(swaggerDocument));
 
 mealRoute.get('/meals/', MealController.getAllMeals);
 mealRoute.post('/meals/', MealMiddleware.postMealCheck, MealController.postMeal);
